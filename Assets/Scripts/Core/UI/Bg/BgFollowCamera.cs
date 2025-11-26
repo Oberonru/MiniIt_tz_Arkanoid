2bc40b55b4b16632af3a093a246ec7e579231bd2
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-11-26 16:03:18 +0600

    Edit background position

---
+++ Assets/Scripts/Core/UI/Bg/BgFollowCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Core.UI.Bg
+{
+    public class BgFollowCamera : MonoBehaviour
+    {
+        private void Start()
+        {
+            transform.position = Camera.main.transform.position + new Vector3(0, 0, 10);
+        }
+    }
+}
